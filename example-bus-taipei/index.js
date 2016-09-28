180a7ae819897189f0b4632425b3288f2ae0f7b9
marc.wang <s4161035@gmail.com>
2016-09-28 17:08:24 +0800

    Update request bus information by name

--- example-bus-taipei/index.js
+++ example-bus-taipei/index.js
@@ -1,51 +1,39 @@
 const fs = require('fs');
 const zlib = require('zlib')
 const request = require('request');
+const Immutable = require('immutable');
+
+let TPERouteList = new Map();
+let TPERouteNameMap = new Map();
+let TPERouteStopList = new Map();
+let TPERouteStopNameMap = new Map();
 
 function getRoute() {
-    return new Promise(function(resolve, reject) {
+    return new Promise((resolve, reject) => {
         request({
             url: 'http://data.taipei/bus/ROUTE',
             method: 'GET',
             encoding: null
-        }, function(err, response, body) {
+        }, (err, response, body) => {
             if (err) {
                 return reject(err);
             } else {
                 zlib.gunzip(body, function(err, result) {
                     if (err) return console.error(err);
-
                     const jsonData = JSON.parse(result.toString());
-                    console.log(jsonData.EssentialInfo);
-                    console.log(jsonData.BusInfo[0])
-
-                });
-            }
-        });
-    });
-};
-
-function getEstimateTime() {
-    return new Promise(function(resolve, reject) {
-        request({
-            url: 'http://data.taipei/bus/EstimateTime',
-            method: 'GET',
-            encoding: null
-        }, function(err, response, body) {
-            if (err) {
-                return reject(err);
-            } else {
-                zlib.gunzip(body, function(err, result) {
-                    if (err) return console.error(err);
-
-                    const jsonData = JSON.parse(result.toString());
-                    // console.log(jsonData.EssentialInfo);
-                    // console.log(jsonData.BusInfo[0])
                     for (let i = 0; i < jsonData.BusInfo.length; i++) {
-                        if( jsonData.BusInfo[i].RouteID == 16184)
-                            console.log(jsonData.BusInfo[i]);
+                        let name = jsonData.BusInfo[i].nameZh;
+                        let id = jsonData.BusInfo[i].Id;
+                        let data = jsonData.BusInfo[i];
+                        let value = [];
+                        if (TPERouteList.has(id)) {
+                            value = TPERouteList.get(id);
+                        }
+                        value.push(data);
+                        TPERouteList.set(id, value);
+                        TPERouteNameMap.set(name, id);
                     }
-
+                    resolve();
                 });
             }
         });
@@ -53,42 +41,109 @@ function getEstimateTime() {
 };
 
 function getStop() {
-    return new Promise(function(resolve, reject) {
+    return new Promise((resolve, reject) => {
         request({
             url: 'http://data.taipei/bus/Stop',
             method: 'GET',
             encoding: null
-        }, function(err, response, body) {
+        }, (err, response, body) => {
             if (err) {
                 return reject(err);
             } else {
                 zlib.gunzip(body, function(err, result) {
                     if (err) return console.error(err);
-
                     const jsonData = JSON.parse(result.toString());
-                    // console.log(jsonData.EssentialInfo);
-                    // console.log(jsonData.BusInfo[0])
-                    // for (let i = 0; i < jsonData.BusInfo.length; i++) {
-                    //     if( jsonData.BusInfo[i].RouteID == 16184)
-                    //         console.log(jsonData.BusInfo[i]);
-                    // }
-
+                    for (let i = 0; i < jsonData.BusInfo.length; i++) {
+                        let id = jsonData.BusInfo[i].Id;
+                        let name = jsonData.BusInfo[i].nameZh;
+                        let data = jsonData.BusInfo[i];
+                        TPERouteStopNameMap.set(id, name);
+                        TPERouteStopList.set(id, data)
+                    }
+                    resolve();
                 });
             }
         });
     });
 };
 
+function translateTime(sec) {
+    if (sec > 60) {
+        let m = Math.floor(sec / 60);
+        let s = sec % 60;
+        return `${m} 分 ${s} 秒`
+    } else {
+        let s = sec;
+        return `${s} 秒`;
+    }
+}
+
+function getEstimateTime(name) {
+    return new Promise((resolve, reject) => {
+
+        let id = TPERouteNameMap.get(name);
+        console.log(`id = ${id} , name: ${name}`);
+
+        request({
+            url: 'http://data.taipei/bus/EstimateTime',
+            method: 'GET',
+            encoding: null
+        }, (err, response, body) => {
+            if (err) {
+                return reject(err);
+            } else {
+                zlib.gunzip(body, function(err, result) {
+                    if (err) return console.error(err);
+                    const jsonData = JSON.parse(result.toString());
+                    for (let i = 0; i < jsonData.BusInfo.length; i++) {
+                        let routeId = jsonData.BusInfo[i].RouteID;
+                        if (routeId == id) {
+                            let stopId = jsonData.BusInfo[i].StopID;
+                            let estimateTime = jsonData.BusInfo[i].EstimateTime;
+                            let stopName = TPERouteStopNameMap.get(stopId);
+                            let goBack = jsonData.BusInfo[i].GoBack;
+                            if( goBack == 0 ){
+                            	console.log(`Name: ${stopName}, Time: ${translateTime(estimateTime)}`);
+                            }
+                            else{
+
+                            }
+                        }
+                    }
+                });
+            }
+        });
+    });
+};
+
+
+
 getRoute()
-    .then(() => {})
-
-getStop()
     .then(() => {
+        // TPERouteList.forEach((value, key, map) => {
+        //     for (let i = 0; i < value.length; i++) {
+        //     	console.log(`id = ${key} , ${value[i].pathAttributeName}`);
+        //     }
+        // });
+        // TPERouteNameMap.forEach((value, key, map) => {
+        //     console.log(`name = ${key}, id = ${value}`);
+        // });
+        getStop()
+            .then(() => {
+                // TPERouteStopList.forEach((value, key, map) => {
+                //     console.log(`id = ${key} , ${value.nameZh}`);
+                // })
+                setInterval(() => {
+                    let busName = '518';
+                    getEstimateTime(busName)
+                        .then(() => {})
+                }, 10*1000)
+            })
     })
 
-getEstimateTime()
-    .then(() => {
-    })
+
+
+
 
 
 // downloadAndUnzip('http://data.taipei/bus/EstimateTime')
